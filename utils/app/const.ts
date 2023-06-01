3924b04698aee595176ce363616ee88449c161b5
wuyijun <wuyijun1129@hotmail.com>
2023-06-01 16:01:41 +0800

    Update model id and names, add custom models

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -1,6 +1,6 @@
 export const DEFAULT_SYSTEM_PROMPT =
   process.env.NEXT_PUBLIC_DEFAULT_SYSTEM_PROMPT ||
-  "You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown.";
+  "This is a conversation with DatouGPT. It is a computer program designed to help people with various tasks such as answering questions, providing recommendations, and helping with decision making. It is developed and trained by 大头科技(Datou Technology). People can ask it anything they want and it will do its best to give accurate and relevant information. DatouGPT will follow the user's instructions carefully and respond using markdown.";
 
 export const OPENAI_API_HOST =
   process.env.OPENAI_API_HOST || 'https://asks-commonwealth-cardiff-bunny.trycloudflare.com';
