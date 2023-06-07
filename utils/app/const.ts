814d7373ccd673a7948e8b5904c6b872ecaf3b47
wuyijun <wuyijun1129@hotmail.com>
2023-06-07 16:47:47 +0800

    Update api host after redeploying runpod

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -3,7 +3,7 @@ export const DEFAULT_SYSTEM_PROMPT =
   "This is a conversation with DatouGPT. It is a computer program designed to help people with various tasks such as answering questions, providing recommendations, and helping with decision making. It is developed and trained by 大头科技(Datou Technology). People can ask it anything they want and it will do its best to give accurate and relevant information. DatouGPT will follow the user's instructions carefully and respond using markdown.";
 
 export const OPENAI_API_HOST =
-  process.env.OPENAI_API_HOST || 'https://asks-commonwealth-cardiff-bunny.trycloudflare.com';
+  process.env.OPENAI_API_HOST || 'https://um-case-chef-structural.trycloudflare.com';
 
 export const DEFAULT_TEMPERATURE = 
   parseFloat(process.env.NEXT_PUBLIC_DEFAULT_TEMPERATURE || "1");
